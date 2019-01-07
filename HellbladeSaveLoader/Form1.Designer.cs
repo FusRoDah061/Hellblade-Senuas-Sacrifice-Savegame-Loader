acafc75ec0c070533a2d0fd8d9fbc24a4e163fb0
Allex Rodrigues <allexxrodriguess@gmail.com>
2019-01-06 23:10:06 -0200

    Finished GUI

--- HellbladeSaveLoader/Form1.Designer.cs
+++ HellbladeSaveLoader/Form1.Designer.cs
@@ -28,13 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnBackup = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbxSavefiles = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnBackup
+            // 
+            this.btnBackup.Location = new System.Drawing.Point(12, 12);
+            this.btnBackup.Name = "btnBackup";
+            this.btnBackup.Size = new System.Drawing.Size(160, 23);
+            this.btnBackup.TabIndex = 0;
+            this.btnBackup.Text = "Backup current savefile";
+            this.btnBackup.UseVisualStyleBackColor = true;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 96);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Choose a savefile to load";
+            // 
+            // cbxSavefiles
+            // 
+            this.cbxSavefiles.FormattingEnabled = true;
+            this.cbxSavefiles.Location = new System.Drawing.Point(12, 69);
+            this.cbxSavefiles.Name = "cbxSavefiles";
+            this.cbxSavefiles.Size = new System.Drawing.Size(160, 21);
+            this.cbxSavefiles.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(193, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 78);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Do a backup before loading a new savefile. If you don\'t you will loose that savef" +
+    "ile, because it will be overwritten by the new one.";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(333, 129);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbxSavefiles);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnBackup);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnBackup;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbxSavefiles;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
     }
 }
 
